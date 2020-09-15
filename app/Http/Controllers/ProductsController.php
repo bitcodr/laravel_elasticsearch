b655740b9da6880e33ab5803bb3c05cd7fce48bb
Amirali <bitcodr@gmail.com>
2020-09-15 20:34:25 +0430

    fixed

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -1,134 +1,91 @@
 <?php namespace App\Http\Controllers;
 
-use App\Http\Requests\ProductRequest;
 use App\Http\Requests\SearchRequest;
 use App\Models\products;
-use ElasticScoutDriverPlus\SearchResult;
+use Elasticsearch\ClientBuilder;
 
 class ProductsController extends Controller
 {
-    public function insert(ProductRequest $request)
-    {
-        $product = new products();
 
-        $product->title = $request->input("title");
-        $product->size = $request->input("size");
-        $product->price = $request->input("price");
-        $product->designer = $request->input("designer");
-        $product->summary = $request->input("summary");
-        $product->thumbnail = $request->input("thumbnail");
-        $product->product_id = $request->input("product_id");
-        $product->tags = $request->input("tags");
-
-        $product->save();
-    }
-
-    //TODO create an elastic helper and improve queries, one query at once
     public function search(SearchRequest $request)
     {
         if (!empty($request->query("field"))) {
             return $this->exactOnSpecificField($request);
         }
 
-        $searchResult = $this->exactMatch($request);
-
-        if ($searchResult->total() < 1) {
-
-            $searchResult = $this->prefixMatch($request);
-            $array = array_unique(array_column($this->flattenSearchDocument($searchResult), "title"));
-            $fuzzyProducts = $this->fuzzyProductsMatch($request);
-
-            if ($searchResult->total() < 1) {
-
-                $designerSuggest = $this->suggestionMatch('designer', $request);
-                $designers = array_unique(array_column($this->flattenSearchDocument($designerSuggest), "designer"));
-
-                $titleSuggest = $this->suggestionMatch('title', $request);
-                $titles = array_unique(array_column($this->flattenSearchDocument($titleSuggest), "title"));
-
-                return $this->makeResponse($request, "did_you_mean", [], $designers, $titles, $this->flattenSearchDocument($fuzzyProducts));
-            }
-
-            return $this->makeResponse($request, "suggestion", $array, [], [], $this->flattenSearchDocument($fuzzyProducts));
-        }
-
-        return $this->makeResponse($request, "products", [], [], [], $this->flattenSearchDocument($searchResult));
+        return $this->find($request);
     }
 
 
     private function exactOnSpecificField(SearchRequest $request)
     {
-        switch ($request->query("field")) {
-            case "designer":
-                $fieldName = "designer";
-                break;
-            case "name":
-                $fieldName = "title";
-                break;
-            default:
-                return response()->json(["message" => "field must be one of the designer or name"]);
-        }
+        $fieldName = $request->query("field");
+        $fieldName = ($fieldName == "name") ? "title" : $fieldName;
 
         $searchResult = products::boolSearch()
             ->must('match', [$fieldName => $request->query("q")])
             ->execute();
-        return $this->makeResponse($request, "products", [], [], [], $this->flattenSearchDocument($searchResult));
+
+        $array = [];
+        foreach ($searchResult->documents() as $doc) {
+            $array[] = $doc->getContent();
+        }
+
+        return $this->makeResponse($request, "products", [], [], [], $array);
     }
 
-    private function exactMatch(SearchRequest $request): SearchResult
-    {
-        return products::boolSearch()
-            ->must('match', ['title' => $request->query("q")])
-            ->execute();
-    }
 
-    private function prefixMatch(SearchRequest $request): SearchResult
+    private function find($request)
     {
-        return products::boolSearch()
-            ->must('prefix', ['title' => $request->query("q")])
-            ->execute();
-    }
-
-    private function suggestionMatch(string $field, SearchRequest $request): SearchResult
-    {
-        return products::boolSearch()
-            ->must('match', [$field => [
-                'query' => $request->query("q"),
-                'fuzziness' => 5
-            ]])
-            ->execute();
-    }
-
-    private function fuzzyProductsMatch(SearchRequest $request): SearchResult
-    {
-        return products::boolSearch()
-            ->shouldRaw([[
-                'multi_match' => [
-                    "query" => $request->query("q"),
-                    "type" => "phrase",
-                    "fields" => [
-                        "title",
-                        "designer",
-                        "summary",
-                        "tags"
-                    ],
-                    "boost" => 10
-                ]
+        $params = [
+            'body' => [
+                ["index" => "products"],
+                ["query" => ["bool" => ["must" => [["match" => ["title" => $request->query("q")]]]]]],
+                ["index" => "products"],
+                ["query" => ["bool" => ["must" => [["prefix" => ["title" => $request->query("q")]]]]]],
+                ["index" => "products"],
+                ["query" => ["bool" => ["must" => [["match" => ["title" => ["query" => $request->query("q"), "fuzziness" => 5]]]]]]],
+                ["index" => "products"],
+                ["query" => ["bool" => ["must" => [["match" => ["designer" => ["query" => $request->query("q"), "fuzziness" => 5]]]]]]],
+                ["index" => "products"],
+                ["query" => ["bool" => ["should" => [["multi_match" => ["query" => $request->query("q"), "type" => "phrase", "fields" => ["title", "designer", "summary", "tags"], "boost" => 10]], ["multi_match" => ["query" => $request->query("q"), "type" => "most_fields", "fields" => ["title", "designer", "summary", "tags"], "fuzziness" => 5]]]]]]
             ],
-                [
-                    'multi_match' => [
-                        "query" => $request->query("q"),
-                        "type" => "most_fields",
-                        "fields" => [
-                            "title",
-                            "designer",
-                            "summary",
-                            "tags"
-                        ],
-                        "fuzziness" => 5
-                    ]
-                ]])->execute();
+        ];
+
+        $client = ClientBuilder::create()->build();
+        $result = $client->msearch($params);
+        return $this->flattenSearchDocument($request, $result);
     }
+
+
+    private function flattenSearchDocument(SearchRequest $request, array $result)
+    {
+        //we performed 5 query and use the indexes to get each query data
+        if (isset($result['responses']) && count($result['responses']) !== 5) {
+            return response()->json(["message" => "cannot find any result"]);
+        }
+
+        $exactMatch = $result['responses'][0]["hits"]["hits"] ?? [];
+        $suggestion = $result['responses'][1]["hits"]["hits"] ?? [];
+        $title_typo_fix = $result['responses'][2]["hits"]["hits"] ?? [];
+        $designer_typo_fix = $result['responses'][3]["hits"]["hits"] ?? [];
+        $products = $result['responses'][4]["hits"]["hits"] ?? [];
+
+        $reference = (count($exactMatch) > 0) ? "products" : (
+            (count($suggestion) > 0) ? "suggestion" : (
+                (count($title_typo_fix) > 0 || count($designer_typo_fix) > 0) ? "did_you_mean" : ""
+            )
+        );
+
+        return $this->makeResponse($request,
+            $reference,
+            (count($exactMatch) == 0) ? array_column($suggestion, "_source") : [],
+            (count($exactMatch) == 0 && count($suggestion) == 0) ? array_column($designer_typo_fix, "_source") : [],
+            (count($exactMatch) == 0 && count($suggestion) == 0) ? array_column($title_typo_fix, "_source") : [],
+            (count($exactMatch) > 0) ? array_column($exactMatch, "_source")
+                : array_column($products, "_source"));
+    }
+
 
     //TODO use laravel resources and collections
     private function makeResponse(SearchRequest $request,
@@ -150,14 +107,4 @@ class ProductsController extends Controller
         ]);
     }
 
-    private function flattenSearchDocument(SearchResult $result): array
-    {
-        $array = [];
-
-        foreach ($result->documents() as $doc){
-            $array[] = $doc->getContent();
-        }
-
-        return $array;
-    }
 }
